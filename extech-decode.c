a632d6fc9b67dcfdd63c69ddd2121d4eba722543
Andrew Sharp <andy@tigerand.com>
2017-07-21 19:51:04 -0700

    fix bug in extech-decode where buffer was 5 but we were reading 20+ bytes into it. add hexout option, where the packet is printed out before it's decoded.  like od 	and extech-decode programs had a baby.

--- extech-decode.c
+++ extech-decode.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include "extech.h"
+
+#undef debugp
+#define debugp(A, B...) fprintf(stdout, A, B)
 
  int
 decode_extech_value(unsigned char byt3, unsigned char byt4, char *a)
@@ -72,18 +76,59 @@ error_exit:
 	return -1;
 }
 
+ void
+print_block(char *b, int bcount)
+{
+	if ((bcount == 0) || (bcount > 4)) {
+		debugp("%.2hhx %02hhx %02hhx %02hhx %02hhx", b[0], b[1], b[2], b[3],
+			b[4]);
+	} else if (bcount == 1) {
+		debugp("%.2hhx", b[0]);
+	} else if (bcount == 2) {
+		debugp("%.2hhx %02hhx", b[0], b[1]);
+	} else if (bcount == 3) {
+		debugp("%.2hhx %02hhx %02hhx", b[0], b[1], b[2]);
+	} else if (bcount == 4) {
+		debugp("%.2hhx %02hhx %02hhx %02hhx", b[0], b[1], b[2], b[3]);
+	}
+}
 
-main() {
-	unsigned char buf[5];
+
+main(int argc, char **argv) {
+	unsigned char buf[64];
 	char out[32];
 	unsigned int rc;
+	int hexout;
 
-	while (read(0, buf, 20)) {
+	hexout = 0;
+	if (argv[1] && (!strncmp("-x", argv[1], 2))) {
+		hexout = 1;
+	}
+
+	/* output in watts,pf,volts,amps order */
+	while (rc = read(0, buf, 20)) {
+printf("read returned %d  ", rc);
+		if (rc < 20) {
+			if (rc == 1) {
+				printf("'%hhx'\n", buf[0]);
+			}
+			break;
+		}
+		if (hexout) {
+			print_block(&buf[0], 5);
+			printf("|");
+			print_block(&buf[15], 5);
+			printf("|");
+			print_block(&buf[10], 5);
+			printf("|");
+			print_block(&buf[5], 5);
+			printf("\t");
+		}
 		rc = decode_extech_value(buf[2], buf[3], out);
 		if (rc >= 0) {
 			printf("%s ", out);
 		}
-		rc = decode_extech_value(buf[5 + 2], buf[5 + 3], out);
+		rc = decode_extech_value(buf[15 + 2], buf[15 + 3], out);
 		if (rc >= 0) {
 			printf("%s ", out);
 		}
@@ -91,7 +136,7 @@ main() {
 		if (rc >= 0) {
 			printf("%s ", out);
 		}
-		rc = decode_extech_value(buf[15 + 2], buf[15 + 3], out);
+		rc = decode_extech_value(buf[5 + 2], buf[5 + 3], out);
 		if (rc >= 0) {
 			printf("%s", out);
 		}
